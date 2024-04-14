f285754c25a9c6f9e38081196e127712d5633ed0
Akash Chand <chandakash958@gmail.com>
2024-04-14 15:33:41 +0530

    Celebrity Data Created on next folder constant

---
+++ lms-frontend/src/Constants/CelebrityData.js
@@ -0,0 +1,40 @@
+import apj from "../assets/Images/QuotesPersonalityImage/apj.png";
+import billGates from "../assets/Images/QuotesPersonalityImage/billGates.png";
+import einstein from "../assets/Images/QuotesPersonalityImage/einstein.png";
+import nelson from "../assets/Images/QuotesPersonalityImage/nelsonMandela.png";
+import steveJobs from "../assets/Images/QuotesPersonalityImage/steveJobs.png";
+
+export const celebrities =[
+    {
+        title:"Nelson Mandela",
+        description:" Remember that hope is a powerful weapon even when all is lost ",
+        image:nelson,
+        slideNumber:1
+    },
+    {
+      title:"APJ Abdul Kalam",
+      description:" If you want to shine like a sun , first burn like a sun ",
+      image:apj,
+      slideNumber:2
+    },
+    {
+      title:"Albert Einstein",
+      description:" Imagination is more important than knowledge  ",
+      image:einstein,
+      slideNumber:3
+    },
+    {
+      title:"Steve Jobs",
+      description:" Don't let the others opinions drawn out your inner voice  ",
+      image:steveJobs,
+      slideNumber:4
+    },
+    {
+      title:"Bill gates",
+      description:" Remember that hope is a powerful weapon even when all is lost ",
+      image:billGates,
+      slideNumber:5
+  },
+
+]
+
--- lms-frontend/src/Pages/AboutUs.jsx
+++ lms-frontend/src/Pages/AboutUs.jsx
@@ -1,48 +1,12 @@
 import aboutMainImage from "../assets/Images/aboutMainImage.png";
-import apj from "../assets/Images/QuotesPersonalityImage/apj.png";
-import billGates from "../assets/Images/QuotesPersonalityImage/billGates.png";
-import einstein from "../assets/Images/QuotesPersonalityImage/einstein.png";
-import nelson from "../assets/Images/QuotesPersonalityImage/nelsonMandela.png";
-import steveJobs from "../assets/Images/QuotesPersonalityImage/steveJobs.png";
+import { celebrities } from "../Constants/CelebrityData";
 import CarouselSlide from "../Components/CarouselSlide";
 
 import HomeLayout from "../Layouts/HomeLayout";
 
 function AboutUs() {
  
-  const celebrities =[
-    {
-        title:"Nelson Mandela",
-        description:" Remember that hope is a powerful weapon even when all is lost ",
-        image:nelson,
-        slideNumber:1
-    },
-    {
-      title:"APJ Abdul Kalam",
-      description:" If you want to shine like a sun , first burn like a sun ",
-      image:apj,
-      slideNumber:2
-    },
-    {
-      title:"Albert Einstein",
-      description:" Imagination is more important than knowledge  ",
-      image:einstein,
-      slideNumber:3
-    },
-    {
-      title:"Steve Jobs",
-      description:" Don't let the others opinions drawn out your inner voice  ",
-      image:steveJobs,
-      slideNumber:4
-    },
-    {
-      title:"Bill gates",
-      description:" Remember that hope is a powerful weapon even when all is lost ",
-      image:billGates,
-      slideNumber:5
-  },
-
-]
+   
 // console.log(celebrities.length);
  
  
