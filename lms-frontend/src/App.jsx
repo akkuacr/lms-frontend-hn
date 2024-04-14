05278d3115d83ce074ee93d68a3b4e1e6be48728
Akash Chand <chandakash958@gmail.com>
2024-04-14 15:59:59 +0530

    Added 404 Not found page

--- lms-frontend/src/App.jsx
+++ lms-frontend/src/App.jsx
@@ -6,6 +6,7 @@ import HomePage from './Pages/HomePage'
 import AboutUs from './Pages/AboutUs' 
 
 import { Route, Routes } from 'react-router-dom'
+import NotFound from './Pages/NotFound'
  
  
 
@@ -18,6 +19,7 @@ function App() {
          <Routes>
              <Route path="/" element={<HomePage/>}></Route>
              <Route path="/about" element={<AboutUs/>}></Route>
+             <Route  path="/*" element={<NotFound/>}></Route>
          </Routes>
          
        
