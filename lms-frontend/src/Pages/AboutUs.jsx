e74676098cf77d50c86f25ad194a026a573e8c35
Akash Chand <chandakash958@gmail.com>
2024-04-14 15:25:10 +0530

    Carousel Slide done

--- lms-frontend/src/Pages/AboutUs.jsx
+++ lms-frontend/src/Pages/AboutUs.jsx
@@ -4,10 +4,48 @@ import billGates from "../assets/Images/QuotesPersonalityImage/billGates.png";
 import einstein from "../assets/Images/QuotesPersonalityImage/einstein.png";
 import nelson from "../assets/Images/QuotesPersonalityImage/nelsonMandela.png";
 import steveJobs from "../assets/Images/QuotesPersonalityImage/steveJobs.png";
+import CarouselSlide from "../Components/CarouselSlide";
 
 import HomeLayout from "../Layouts/HomeLayout";
 
 function AboutUs() {
+ 
+  const celebrities =[
+    {
+        title:"Nelson Mandela",
+        description:" Remember that hope is a powerful weapon even when all is lost ",
+        image:nelson,
+        slideNumber:1
+    },
+    {
+      title:"APJ Abdul Kalam",
+      description:" If you want to shine like a sun , first burn like a sun ",
+      image:apj,
+      slideNumber:2
+    },
+    {
+      title:"Albert Einstein",
+      description:" Imagination is more important than knowledge  ",
+      image:einstein,
+      slideNumber:3
+    },
+    {
+      title:"Steve Jobs",
+      description:" Don't let the others opinions drawn out your inner voice  ",
+      image:steveJobs,
+      slideNumber:4
+    },
+    {
+      title:"Bill gates",
+      description:" Remember that hope is a powerful weapon even when all is lost ",
+      image:billGates,
+      slideNumber:5
+  },
+
+]
+// console.log(celebrities.length);
+ 
+ 
   return (
     <HomeLayout>
       <div className="pl-20 pt-20 flex flex-col text-white  ">
@@ -39,113 +77,12 @@ function AboutUs() {
         {/* Carousel Start */}
 
         <div className="carousel w-1/2 my-16 m-auto ">
-          
-          <div id="slide1" className="carousel-item relative w-full">
-            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-              <img
-                src={apj}
-                className="w-40 rounded-full border-2 border-gray-400 "
-              />
-              <p className="text-xl text-gray-200">
-                {" If you want to shine like a sun , first burn like a sun "}
-              </p>
-              <h3 className="text-2xl font-semibold">APJ Abdul Kalam</h3>
-              <div className="absolute  flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide5" className="btn btn-circle">
-                  ❮
-                </a>
-                <a href="#slide2" className="btn btn-circle">
-                  ❯
-                </a>
-              </div>
-            </div>
-          </div>
-          <div id="slide2" className="carousel-item relative w-full">
-            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-              <img
-                src={billGates}
-                className="w-40 rounded-full border-2 border-gray-400 "
-              />
-               <p className="text-xl text-gray-200">
-                {"  Success is a lousy teacher. It seduces smart people into thinking they can't lose "}
-              </p>
-              <h3 className="text-2xl font-semibold">Bill Gates</h3>
-              <div className="absolute  flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide1" className="btn btn-circle">
-                  ❮
-                </a>
-                <a href="#slide3" className="btn btn-circle">
-                  ❯
-                </a>
-              </div>
-            </div>
-          </div>
-          <div id="slide3" className="carousel-item relative w-full">
-            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-              <img
-                src={einstein}
-                className="w-40 rounded-full border-2 border-gray-400 "
-              />
-               
-               <p className="text-xl text-gray-200">
-                {" Imagination is more important than knowledge "}
-              </p>
-              <h3 className="text-2xl font-semibold">Albert Einstein</h3>
-
-              <div className="absolute  flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide2" className="btn btn-circle">
-                  ❮
-                </a>
-                <a href="#slide4" className="btn btn-circle">
-                  ❯
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div id="slide4" className="carousel-item relative w-full">
-            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-              <img
-                src={nelson}
-                className="w-40 rounded-full border-2 border-gray-400 "
-              />
-               <p className="text-xl text-gray-200">
-                {"   Remember that hope is a powerful weapon even when all is lost "}
-              </p>
-              <h3 className="text-2xl font-semibold">Nelson Mandela</h3>
-              <div className="absolute  flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide3" className="btn btn-circle">
-                  ❮
-                </a>
-                <a href="#slide5" className="btn btn-circle">
-                  ❯
-                </a>
-              </div>
-            </div>
-          </div>
-          <div id="slide5" className="carousel-item relative w-full">
-            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-              <img
-                src={steveJobs}
-                className="w-40 rounded-full border-2 border-gray-400 "
-              />
-              
-              <p className="text-xl text-gray-200">
-                {"Don't let the others opinions drawn out your inner voice "}
-              </p>
-              <h3 className="text-2xl font-semibold">Steve Jobs</h3>
-
-
-              <div className="absolute  flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide4" className="btn btn-circle">
-                  ❮
-                </a>
-                <a href="#slide1" className="btn btn-circle">
-                  ❯
-                </a>
-              </div>
-            </div>
-          </div>
+        console.log(celebrities.length);
+          {celebrities && celebrities.map(celebrity =>(<CarouselSlide
+                                                         {...celebrity}
+                                                         key={celebrity.slideNumber}
+                                                         totalSlides={celebrities.length}
+                                                       /> ) ) }
 
         </div>
 
